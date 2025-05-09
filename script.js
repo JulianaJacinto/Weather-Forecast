c9e4e0c3e40d711986b16b10bb5964cfdd90f78c
Juliana Jacinto <julimaria2003@gmail.com>
2025-05-09 18:09:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("themeToggle");
+  const body = document.body;
+
+  toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-theme");
+  });
+});
